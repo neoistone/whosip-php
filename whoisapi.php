42fef974c66288e02e708c57df92ce7f0025ec2d
Neoistone india <59179052+neoistone@users.noreply.github.com>
2021-02-05 01:08:53 +0530

    Create whoisapi.php

---
+++ whoisapi.php
@@ -0,0 +1,30 @@
+<?php
+
+
+namespace neoistone\api;
+
+
+class neoistone_whois
+{
+  private  $api = "https://restapi.neoistone.com/v2/whois/ip/";
+
+    /**
+     * neoistone_whois constructor.
+     * @param $ip
+     * @param $type
+     */
+    public function __construct($ip,$type)
+    {
+        $this->api .= $ip.'?type='.$type;
+    }
+
+    public function info(){
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL,$this->api);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $server_output = curl_exec($ch);
+        curl_close ($ch);
+        return json_decode($server_output,1);
+    }
+
+}
