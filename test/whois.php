049cf594e92aad4ec678aa3286f1cd4cabf75314
Neoistone india <59179052+neoistone@users.noreply.github.com>
2021-02-05 01:55:42 +0530

    Update whois.php

--- test/whois.php
+++ test/whois.php
@@ -1,7 +1,26 @@
 <?php
 
-include 'vendor/autoloader.php';
 
-$obj = new neoistone\api\whois_ip('1.1.1.1');
+class whois_ip
+{
+  private  $api = "https://restapi.neoistone.com/v2/whois/ip/";
 
-print($obj->info());
+    /**
+     * whois_ip constructor.
+     * @param $ip
+     * @param $type
+     */
+    public function __construct($ip,$type)
+    {
+        $this->api .= $ip.'?type='.$type;
+    }
+
+    public function info(){
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL,$this->api);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $server_output = curl_exec($ch);
+        curl_close ($ch);
+        return json_decode($server_output,1);
+    }
+}
